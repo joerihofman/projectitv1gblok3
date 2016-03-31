b9ad81bd23af5786941bc98fa52391426cd96f38
SentientTurtle <danny.oldenburger@gmail.com>
2016-03-31 08:53:22 +0200

    Added main

---
+++ Project/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Generic main starting class
+ */
+public class Main {
+    /**
+     * Generic 'main' method
+     * @param args commandline arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
