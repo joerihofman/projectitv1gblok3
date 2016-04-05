20cea882dea71c1bc6b4ae91897c3968cb3682c4
qwerty12794 <onzin09@gmail.com>
2016-04-05 12:14:49 +0200

    Pie chart added, not implemented correctly

---
+++ src/yellowsparkle/Piechart/MakePiechart.java
@@ -0,0 +1,33 @@
+package yellowsparkle.Piechart;
+
+/**
+ * Created by MSI on 5-4-2016.
+ */
+import javax.swing.*;
+
+public class MakePiechart  {
+    private Model model;
+    private JFrame screen;
+    private View countview;
+    private View pieview;
+    private Controller controller;
+
+    public MakePiechart() {
+        model=new Model();
+        controller=new Controller(model);
+        countview=new CountView(model);
+        pieview=new PieView(model);
+        screen=new JFrame("Model View Controller/Dynamic Model with thread");
+        screen.setSize(450, 285);
+        screen.setResizable(false);
+        screen.setLayout(null);
+        screen.getContentPane().add(countview);
+        screen.getContentPane().add(pieview);
+        screen.getContentPane().add(controller);
+        countview.setBounds(10, 10, 200, 200);
+        pieview.setBounds(230, 10, 200, 200);
+        controller.setBounds(0, 210, 450, 50);
+        screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        screen.setVisible(true);
+    }
+}
