6eb3a930b457046309667ab553b1f8ff1feb7654
Joost Holwerda <jo.holwerda@st.hanze.nl>
2016-04-07 11:48:30 +0200

    comments added

--- src/yellowsparkle/parking/simulation/Simulator.java
+++ src/yellowsparkle/parking/simulation/Simulator.java
@@ -25,15 +25,14 @@ public class Simulator {
 
     public Simulator(Garage garage) {
         this.garage = garage;
-        entryQueue = new ArrayDeque<>();
+        entryQueue = new ArrayDeque<>(); //Deque is for the queues to add/remove one from both sides
         exitQueue = new ArrayDeque<>();
         tickCount = 0;
-
     }
 
     public void tick() throws ParkingException {
-        Date now = new Date();
-        System.out.println("TICK: " + tickCount);
+        Date now = new Date(); //gives a date with each that arrives
+        System.out.println("TICK: " + tickCount); //This is for in the command line to see at which tick we are.
         tickCount++;
 
         if (canExit) {
@@ -43,25 +42,36 @@ public class Simulator {
             }
         }
 
+
         entryQueue.add(new Car(status -> {
             if (status == Car.Status.PARK && (Main.random.nextInt(32) < 4)) return Car.Status.EXIT_WAIT;
             else return status;
-        }, new Ticket(TicketType.REGULAR)));
+        }, new Ticket(TicketType.REGULAR))); //For now all cars has a REGULAR ticket.
 
+        /*
+        * We use boolean isValid to make sure the parking ticket / subscription is still valid.
+        * For now we don't use it but we will get there
+        * */
         for (int i = 0; i < entryPerTick; i++) {
             if (garage.totalSpaces() > garage.getUsedSpaces()) {
-                Car car = entryQueue.peekFirst();
+                Car car = entryQueue.peekFirst(); //peekFirst retrieves the first car in the dequeue.
                 if (car != null) {
                     boolean isValid = true;
-                    Ticket[] tickets = car.getTickets();
+                    Ticket[] tickets = car.getTickets(); //getTickets retrieves all the information about the ticket
+                   /*
+                   * We used a enum for the parking ticket
+                   * Here it checks what kind of parking ticket the car has.
+                   * 7-4-16 11.40 - All the cars has "REGULAR" fornow. This will change.
+                   * */
                     for(Ticket ticket : tickets) {
+                        //Here it searches randomly for the first empty location in the parking garage
                         ParkingSlot spot = garage.getRandomEmptyLocation();
-                        switch(ticket.getType()) {
+                        switch(ticket.getType()) { //asks the type of ticket
                             case REGULAR:
                                 if (spot != null) {
-                                    ticket.setStart(new Date());
+                                    ticket.setStart(new Date()); //When the car is parked this gives it a date to track how long it's parked
                                     spot.setCar(car);
-                                    ticketSold++;
+                                    ticketSold++;  //Here it counts the "REGULAR" tickets sold. This is used in the GUI
                                 } else {
                                     isValid = false;
                                 }
