ffecdeb89a04df5b91259a0f707af92f8b71cc49
Ivo Doppen <ivo1119@live.nl>
2016-04-12 14:32:23 +0200

    Jdoc ParkingException

--- src/yellowsparkle/parking/simulation/ParkingException.java
+++ src/yellowsparkle/parking/simulation/ParkingException.java
@@ -1,6 +1,20 @@
+/**
+ * @author ITV1G Group 1
+ * @version 1.0
+ * @since 4/4/16
+ */
+
 package yellowsparkle.parking.simulation;
 
+/**
+ * Constructor parking exception
+ */
 public class ParkingException extends Exception {
+
+    /**
+     * Sends super message
+     * @param message sents message
+     */
     public ParkingException(String message) {
         super(message);
     }
