5cd63b77643cdf41b044e937a13a1c26d1833c39
qwerty12794 <onzin09@gmail.com>
2016-04-13 13:22:56 +0200

    Added comments

--- src/yellowsparkle/view/types/ParkingSlotCollectionAcceptor.java
+++ src/yellowsparkle/view/types/ParkingSlotCollectionAcceptor.java
@@ -4,6 +4,9 @@ import yellowsparkle.parking.model.ParkingSlot;
 
 import java.util.Collection;
 
+/**
+ * Marker for the views that are using the Parking slot collection
+ */
 public interface ParkingSlotCollectionAcceptor {
     void setParkingSlotCollection(Collection<ParkingSlot> parkingSlotCollection);
 }
