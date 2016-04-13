df88dafbef8a1f9c48e6f336554ac98c7fd4fd58
qwerty12794 <onzin09@gmail.com>
2016-04-13 13:19:41 +0200

    Added comments

--- src/yellowsparkle/parking/model/GarageImpl.java
+++ src/yellowsparkle/parking/model/GarageImpl.java
@@ -11,14 +11,31 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+/**
+ * This class is made for implementing the garage
+ * @author ITV1G Group 1
+ * @version 1.0
+ * @since 31/3/16
+ */
 public class GarageImpl extends Garage {
     private HashMap<Position, ParkingSlot> parkingSlots;
     private Predicate<Car> carPredicate;
 
+    /**
+     * This method is the constructor if not every value is known
+     * @param slots         Parking spaces
+     * @param carPredicate  Parking prediction of the car
+     */
     public GarageImpl(List<ParkingSlot> slots, Predicate<Car> carPredicate) {
         this(slots, carPredicate, null);
     }
 
+    /**
+     * This method is the constructor if every value is known
+     * @param slots         Parking spaces
+     * @param carPredicate  Parking prediction of the car
+     * @param subGarages    Sub garage parking spaces
+     */
     public GarageImpl(List<ParkingSlot> slots, Predicate<Car> carPredicate, List<Garage> subGarages) {
         super(subGarages);
         this.carPredicate = carPredicate;
@@ -27,19 +44,28 @@ public class GarageImpl extends Garage {
     }
 
     /**
-     * @inheritDoc
+     * This method assigns a customer to a parking slot
+     * @param slotConsumer Consumer for slots.
      */
     @Override
     public void forEach(Consumer<ParkingSlot> slotConsumer) {
         parkingSlots.values().forEach(slotConsumer);
     }
 
+    /**
+     * This method assigns every customer to parking slots
+     * @param slotConsumer Consumer for slots
+     */
     @Override
     public void forAll(Consumer<ParkingSlot> slotConsumer) {
         forEach(slotConsumer);
         subGarages.forEach(garage -> garage.forAll(slotConsumer));
     }
 
+    /**
+     * This method gets a random empty parking spot
+     * @return the empty parking spot
+     */
     @Override
     public ParkingSlot getRandomEmptyLocation() {
         List<ParkingSlot> emptyLocations = getEmptyLocations();
@@ -50,11 +76,19 @@ public class GarageImpl extends Garage {
         }
     }
 
+    /**
+     * This method gets the next empty parking spot
+     * @return the empty parking spot
+     */
     @Override
     public List<ParkingSlot> getEmptyLocations() {
         return parkingSlots.values().stream().filter(ParkingSlot::isEmpty).collect(Collectors.toList());
     }
 
+    /**
+     * This method returns the total amount of spaces
+     * @return the amount of spaces
+     */
     @Override
     public Collection<ParkingSlot> getTotalSpaces() {
         return parkingSlots.values();
@@ -70,47 +104,89 @@ public class GarageImpl extends Garage {
         return parkingSlots.values().stream().filter(parkingSlot -> !parkingSlot.isEmpty()).collect(Collectors.toList());
     }
 
+    /**
+     * This method accepts a car
+     * @param car the car
+     * @return a boolean
+     */
     @Override
     public boolean acceptsCar(Car car) {
         return carPredicate.test(car);
     }
 
+    /**
+     * This method can add a car in the garage
+     * @param car      the car
+     * @param position the position of the car in the garage
+     * @return position of the car
+     * @throws ParkingException
+     */
     @Override
     public Car addCar(Car car, Position position) throws ParkingException {
         if (!carPredicate.test(car)) throw new ParkingException("Car not accepted!");
         return parkingSlots.get(position).setCar(car);
     }
 
+    /**
+     * This method removes a car from a parking spot
+     * @param position the position of the car
+     * @return a remove car command
+     */
     @Override
     public Car removeCar(Position position) {
         return parkingSlots.get(position).removeCar();
     }
 
+    /**
+     * This method removes a car from the car list
+     * @param car the car
+     */
     @Override
     public void removeCar(Car car) {
         parkingSlots.values().stream().filter(parkingSlot -> parkingSlot.getCar() == car).forEach(ParkingSlot::removeCar);
     }
 
+    /**
+     * This method checks a key in the position hashmap
+     * @param position the position
+     * @return position
+     */
     @Override
     public boolean hasPosition(Position position) {
         return parkingSlots.containsKey(position);
     }
 
+    /**
+     * This method gets a car from a list
+     * @return a list
+     */
     @Override
     public List<Car> getCars() {
         return parkingSlots.values().stream().map(ParkingSlot::getCar).collect(Collectors.toList());
     }
 
+    /**
+     * This method gets the parking slots from the garage
+     * @return values
+     */
     @Override
     public Collection<ParkingSlot> getParkingSlots() {
         return parkingSlots.values();
     }
 
+    /**
+     * This method removes a car from the list
+     * @return a list
+     */
     @Override
     public List<Car> removeCars() {
         return parkingSlots.values().stream().map(ParkingSlot::removeCar).collect(Collectors.toList());
     }
 
+    /**
+     * This method gets all parking slots
+     * @return the slots
+     */
     @Override
     public Collection<ParkingSlot> getParkingAllSlots() {
         List<ParkingSlot> slots = new ArrayList<>();
